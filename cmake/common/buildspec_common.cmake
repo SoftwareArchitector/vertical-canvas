46b17017726126540a56251d7a48e111b8363e01
Oleksandr Kosenko <software.architector@gmail.com>
2025-05-25 19:48:26 -0400

    1

--- cmake/common/buildspec_common.cmake
+++ cmake/common/buildspec_common.cmake
@@ -210,11 +210,11 @@ function(_check_dependencies)
         file(ARCHIVE_EXTRACT INPUT "${dependencies_dir}/${file}" DESTINATION "${dependencies_dir}")
 
         execute_process(
-          COMMAND ls -la /Users/runner/work/vertical-canvas/vertical-canvas/.deps
+          COMMAND ls -la /Users/runner/work/vertical-canvas/vertical-canvas/.deps/obs-studio-vertical-31.0.3.20250525
           OUTPUT_VARIABLE CHECK_UNPACKER_FILES
           OUTPUT_STRIP_TRAILING_WHITESPACE
         )
-        message(STATUS "Unpacked folder contains: ${CHECK_UNPACKER_FILES}/obs-studio-vertical-31.0.3.20250525")
+        message(STATUS "Unpacked folder contains: ${CHECK_UNPACKER_FILES}")
 
         # Copy files to original OBS folder
 	execute_process(COMMAND cp -r "${dependencies_dir}/obs-studio-vertical-31.0.3.20250525/*" "${dependencies_dir}/obs-studio-31.0.3.20250525")
