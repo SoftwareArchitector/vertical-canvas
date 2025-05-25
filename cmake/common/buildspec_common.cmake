2884b65b84896ad28da16d5caa60562b023a8c1c
Oleksandr Kosenko <software.architector@gmail.com>
2025-05-25 17:13:08 -0400

    1

--- cmake/common/buildspec_common.cmake
+++ cmake/common/buildspec_common.cmake
@@ -68,9 +68,8 @@ function(_setup_obs_studio)
     set(_cmake_version "2.0.0")
   elseif(OS_MACOS)
     message(STATUS "Check SDK path")
-    COMMAND "xcrun --show-sdk-path"
+    COMMAND xcrun --show-sdk-path
 
-    #  
     message(STATUS "Add special cmake variables (${CMAKE_OSX_SYSROOT})")
 
     set(_cmake_generator "Xcode")
